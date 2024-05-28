f397b3b8a12d977411fc07c5b5eee34276971134
Daniel Morris <daniel.2003.morris@gmail.com>
2024-05-28 13:24:18 +1200

    added sidebar

--- my-app/src/components/Sidebar.svelte
+++ my-app/src/components/Sidebar.svelte
@@ -1,6 +1,43 @@
-<aside>
-	<nav>
-		{#each item in navitems}
-		<a href={href}>{Text}</a>
-	</nav>
-</aside>
+<script>
+    let navItems = [
+        { text: "Quiz", href: "/Games/Quizes"},
+		{ text: "Other", href: "/other"}
+    ];
+</script>
+
+<style>
+    .sidebar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 200px;
+        height: 100vh;
+        background-color: whitesmoke;
+        padding: 1em;
+    }
+    .nav-item {
+        cursor: pointer;
+        padding: 0.5em;
+        border-bottom: 1px solid black;
+    }
+    .nav-item:hover {
+        background-color: #e4e4e4;
+    }
+    .nav-link {
+        display: block;
+        padding: 0.5em 1em;
+        text-decoration: none;
+        color: black;
+    }
+    .nav-link:hover {
+        background-color: #ddd;
+    }
+</style>
+
+<div class="sidebar">
+    {#each navItems as item}
+        <div class="nav-item">
+            <a class="nav-link" href="{item.href}">{item.text}</a>
+        </div>
+    {/each}
+</div>
